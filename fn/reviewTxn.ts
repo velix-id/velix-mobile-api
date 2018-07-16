15535eb3b86601d1ccf005cc6b61028f4cde98b1
Balwant Singh <balwant.matharu@gmail.com>
2018-07-16 11:08:05 +1000

    Fixed Format Change

--- fn/reviewTxn.ts
+++ fn/reviewTxn.ts
@@ -1,46 +1,69 @@
 import {HTMLResponse} from "../lib/helpers/html-response";
-import { VelixIDPartner } from "../lib/velixid/partner";
-import { VelixIDAuthRequest, VelixIDAuthRequestStatus } from "../lib/velixid/auth-request";
+import {VelixIDPartner} from "../lib/velixid/partner";
+import {VelixIDAuthRequest, VelixIDAuthRequestStatus} from "../lib/velixid/auth-request";
 
 export const invoke = (event, context, cb) => {
     let response = new HTMLResponse(cb);
-    
-    let partnerToken = event.pathParameters.partnerid.split(':');
-    let txnid = event.pathParameters.txnid;
-    VelixIDPartner.GetPartner(partnerToken[0], partnerToken[1], (error, partner:VelixIDPartner) => {
-        VelixIDAuthRequest.loadRequest(txnid, (txnerr, transaction:VelixIDAuthRequest) => {
-            let html = '';
-            let options:any = {
-                footerstring: 'TXN ' + transaction.requestID
-            };
-            if (error) {
-                html = `<div class="error-message">
-                    <h2>Partner Error</h2>
-                    <p>${error.message}</p>
-                    </div>`;
-            } else {
 
-                let statusMessage = `<div>Please proceed on your app for the next steps.</div>
+    let partnerToken = event
+        .pathParameters
+        .partnerid
+        .split(':');
+
+    let txnid = event.pathParameters.txnid;
+    console.log(txnid);
+    VelixIDPartner.GetPartner(partnerToken[0], partnerToken[1], (error, partner : VelixIDPartner) => {
+        if (error) {
+            let html = `<div class="error-message">
+                <h2>Partner Error</h2>
+                <p>${error.message}</p>
+                </div>`;
+
+            response.popupHTML('Log in with Velix.ID', html, {refresh: 10});
+        } else {
+            VelixIDAuthRequest.loadRequest(txnid, (txnerr, transaction : VelixIDAuthRequest) => {
+                let html = '';
+                let options : any = {
+                    footerstring: 'TXN ' + transaction.requestID
+                };
+                if (txnerr) {
+                    html = `<div class="error-message">
+                    <h2>Partner Error</h2>
+                    <p>${txnerr.message}</p>
+                    </div>`;
+                } else {
+
+                    let statusMessage = `<div>Please proceed on your app for the next steps.</div>
                 <small>Waiting for you to respond on VelixID App.</small>`;
 
+                    if (VelixIDAuthRequestStatus.ACCEPTED === transaction.status) {
+                        let data;
 
-                if (VelixIDAuthRequestStatus.ACCEPTED === transaction.status) {
-                    let data = JSON.parse(transaction.response.data);
-                    statusMessage = `<div class="profile">
+                        try {
+                            if (typeof transaction.response.data === 'string') {
+                                data = JSON.parse(transaction.response.data);
+                            } else {
+                                data = transaction.response.data;
+                            }
+                        } catch (error) {
+                            console.log(error);
+                            console.log(transaction.response);
+                        }
+                        statusMessage = `<div class="profile">
                         <div>Logged in as</div>
                         <img src="data:image/jpeg;base64,${data.image}" />
                         <h3>${data.name.value}</h3>
                     </div>
                     <script>closeMe(${transaction.response.data})</script>`;
-                } else if (VelixIDAuthRequestStatus.REJECTED === transaction.status) {
-                    let data = JSON.parse(transaction.response.data);
-                    statusMessage = `<div>Login <strong>DENIED</strong> by user</div>
+                    } else if (VelixIDAuthRequestStatus.REJECTED === transaction.status) {
+                        let data = JSON.parse(transaction.response.data);
+                        statusMessage = `<div>Login <strong>DENIED</strong> by user</div>
                     <script>closeMe('rejected')</script>`;
-                } else {
-                    options.refresh = 3;
-                }
+                    } else {
+                        options.refresh = 3;
+                    }
 
-                html = `<div class="partner-logo">
+                    html = `<div class="partner-logo">
                         <img src="${partner.logo}" />
                     </div>
                     <p>You are trying to login to:
@@ -48,10 +71,11 @@ export const invoke = (event, context, cb) => {
                     <span class="partner-name">${partner.name}</span>
                     </p>
                     ${statusMessage}
-                    `       
-            }
+                    `
+                }
 
-            response.popupHTML('Log in with Velix.ID', html, options);
-        })
+                response.popupHTML('Log in with Velix.ID', html, options);
+            })
+        }
     });
 }
